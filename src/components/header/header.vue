84217266b8b5896291329370f5371dd8204283ff
mahuaide <mahd_001@sina.com>
2017-03-31 16:41:18 +0800

    路由结构变化，app.vue作为根路由，home.vue作为二级路由

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,27 +2,33 @@
   <div class="header">
     <div class="header-img">
       <span>
-        <img src="./logo.png" alt="亚信" width="38px" height="38px">
+        <img src="./logo.png" alt="logo" width="38px" height="38px">
       </span>
     </div>
     <input type="text" class="search" placeholder="Search..." v-show="this.$store.state.token">
     <div class="header-middle">
-      aaaa
+      <span class="project-name">运营商项目</span>
+      <span class="iconfont icon-arrow_down"></span>
+    </div>
+    <div class="header-right">
+      <span class="iconfont icon-add"></span>
+      <span class="iconfont icon-notify"></span>
+      <div class="header-user">
+        <img src="./logo.png" alt="user" width="38px" height="38px">
+        <span class="iconfont icon-arrow_down"></span>
+      </div>
     </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
   export default{
-      props:{
-
-      }
+    props: {}
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-
-  headerBackGroundColor=#fdfcff
-  fontSize =14px
-  fontFamily="PingFang SC","Helvetica Neue","Hiragino Sans GB","Segoe UI","Microsoft YaHei","微软雅黑",sans-serif;
+  headerBackGroundColor = #fdfcff
+  fontSize = 14px
+  fontFamily = "PingFang SC", "Helvetica Neue", "Hiragino Sans GB", "Segoe UI", "Microsoft YaHei", "微软雅黑", sans-serif;
   .header
     width 100%
     height 60px
@@ -33,13 +39,13 @@
       display table
       float left
       height 100%
-      margin-left 26px
+      margin-left 22  px
       span
         display table-cell
         vertical-align middle
     .search
       position absolute
-      top:18px
+      top: 18px
       margin-left 22px
       width 120px
       border none
@@ -48,14 +54,14 @@
       font-size fontSize
       outline: 0
       font-family fontFamily
-      padding:2px 20px 2px 2px
+      padding: 2px 20px 2px 2px
       vertical-align middle
-      transition  width .4s
+      transition width .4s
       background url('./nav_search.png') no-repeat right center
       background-size 14px 14px
       z-index 10001
       &:focus
-        width:200px
+        width: 200px
         background-image url('./nav_search_focus.png')
         border-bottom-color #8a8a8a
     .header-middle
@@ -63,4 +69,57 @@
       text-align center
       line-height 60px
       z-index 999
+      .project-name
+        color #1a1a1a
+        font-size 14px
+        font-weight bold
+        &:hover
+          cursor pointer
+      .icon-arrow_down
+        font-size 13px
+        &:hover
+          cursor pointer
+    .header-right
+      position absolute
+      z-index 1000
+      right 0px
+      top 0
+      height 60px
+      width 30%
+      text-align right
+      line-height 60px
+      font-size 0
+      .header-user
+        display inline-block
+        position relative
+        padding 0 20px 0 10px
+        height 60px
+        width 60px
+        line-height 60px
+        text-align right
+        &:hover
+          background-color #F2F2F2
+          cursor pointer
+        .icon-arrow_down
+          display inline
+          height 100%
+          font-size 12px
+          margin-left 10px
+        > img
+          position absolute
+          top: 10px
+          right 38px
+          display inline-block
+          border-radius 50%
+      .icon-add, .icon-notify
+        display inline-block
+        text-align center
+        width 60px
+        height 100%
+        font-size 15px
+        color #1a1a1a
+        transition background-color .4s
+        &:hover
+          background-color #F2F2F2
+          cursor pointer
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import mock from './mock'
+import babelPolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
-import axios from './http/http';
+import axios from './http/http'
+import ElementUI from 'element-ui'
 import '../static/css/reset.css'
 import './common/css/common.styl'
 import './common/css/font.css'
+import '../theme/index.css'  //element-UI自定义样式
+
 Vue.prototype.$http = axios
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
@@ -21,7 +26,6 @@ router.beforeEach((to, from, next) => {
   }
   next()
 })
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
