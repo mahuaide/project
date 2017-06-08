ae335ed456f02ce0bcdc12a1a6861b8d6022ac2e
mahuaide <mhd_001@sina.com>
2017-06-08 10:09:03 +0800

    修改写fileServer的bug

--- fileServer.js
+++ fileServer.js
@@ -5,44 +5,49 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
-const port = 9001;
+const PORT = 9001;
 const IP = "localhost";
 
 http.createServer((req, res) => {
   //  /user/login.action  --> user_login.json
-  var pathName = url.parse(req.url).pathname;
-  var fileName = pathName.slice(1).replace('/', '_').replace(path.extname(pathName), '.json');
-  res.setHeader('Content-Type', 'text/json;charset=UTF-8');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Authorization');
+  let pathName = url.parse(req.url).pathname;
+  let fileName = pathName.slice(1).replace(path.extname(pathName), '.json').replace(/\//g, '_');
   fs.readFile(path.join(__dirname, `./src/mock/json/${fileName}`), 'utf-8', (err, data) => {
     if (err) {
-      res.writeHead(404, {});
-      req.end();
+      responseData(404, "");
     } else {
-      if (pathName && req.method !="OPTIONS" && pathName != '/user/login.action' && pathName != '/logout.action') {
+      if (pathName && req.method != "OPTIONS" && pathName != '/user/login.action' && pathName != '/logout.action') {
         var auth = req.headers['authorization'];
-        if(auth != 'Auth0000'){
-          res.writeHead(401, {});
-          res.end("token error");
-        }else {
-          res.writeHead(200, {});
-          res.end(data);
+        if (auth != 'Auth0000') {
+          responseData(401, "token error");
+        } else {
+          responseData(200, data);
         }
-        console.log(auth + "\n" + pathName + "\n" + req.method + "\n" + req.headers['cookie'] + "\n");
-      }else {
-        res.writeHead(200, {});
-        res.end(data);
+      } else {
+        responseData(200, data);
       }
     }
   })
-}).listen(port, IP, (err) => {
+
+  function responseData(status, data) {
+    res.setHeader('Content-Type', 'text/json;charset=UTF-8');
+    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']);
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    setTimeout(() => {
+      res.writeHead(status, {});
+      res.end(data);
+    }, req.method == "OPTIONS" ? 0 : 1000)
+  }
+}).listen(PORT, IP, (err) => {
   if (err) {
     console.log(err);
     return
   }
-  console.log('Listening at http://' + IP + ':' + port + '\n')
+  console.log('Listening at http://' + IP + ':' + PORT + '\n')
 });
+
+
+
 
